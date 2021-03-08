4ffce18ce36c89a8844b6bbfa08c358eaf82a11a
perry leijten <perryleijten@gmail.com>
2021-03-08 17:55:47 +0900

    separated testing

---
+++ unitTest/exec_unitTest.bat
@@ -0,0 +1,29 @@
+@echo off
+rem simple batch file that allows mayapy to load all unittests based on this folder
+
+set /P mayaVersion="What version of maya do you want to test? "
+
+FOR %%G IN ("2017" "2018" "2019" "2020" "2021") DO ( IF /I "%mayaVersion%"=="%%~G" GOTO MATCH )
+
+:NOMATCH
+echo Mayaversion not found
+pause
+GOTO :EOF
+
+:MATCH
+echo Using maya version: %mayaVersion%!
+
+setlocal enabledelayedexpansion
+set baseFolder=%~dp0
+
+set Path2=\unitTest.py
+set Path3=\unitTest\log_file.txt
+
+SET testCommand=%baseFolder:~0,-1%%Path2%
+SET logFile=%baseFolder:~0,-1%%Path3%
+
+ECHO "running %testCommand%"
+
+"C:\Program Files\Autodesk\Maya%mayaVersion%\bin\mayapy.exe" "%testCommand%"
+
+pause
---
+++ unitTest/unitTest.py
@@ -0,0 +1,150 @@
+import unittest, os, sys, logging, cProfile
+
+sys.path.insert(0, os.path.normpath(os.path.join(os.path.abspath(__file__), '../../../..')))
+import maya.standalone
+
+root_logger = logging.getLogger()
+root_logger.disabled = True
+for x, y in logging.Logger.manager.loggerDict.iteritems():
+    try:
+        y.setLevel(logging.CRITICAL)
+    except Exception as e:
+        pass
+
+    ## disable the crash reporting window.
+os.environ["MAYA_DEBUG_ENABLE_CRASH_REPORTING"] = "0"
+os.environ["PYMEL_SKIP_MEL_INIT"] = "Temp"
+
+maya.standalone.initialize(name='python')
+
+# coverage needs to start BEFORE imports happen
+# or else it reports all class definitions as "not covered"
+_coverage = True
+try:
+    import coverage
+    cov = coverage.Coverage(source=['CarbonRigSystem'])
+    cov.start()
+except Exception as err:
+    _coverage = False
+
+from cmdWrapper import cmds
+
+cmds.loadPlugin('matrixNodes',qt=True)
+cmds.loadPlugin('quatNodes', qt=True)
+
+class TestCmds(unittest.TestCase):
+    
+    def basics(self):
+        transform = createNode('transform')
+        self.assertEqual(str(transform), '|transform1')
+        # get and set translate
+        self.assertEqual(transform.tx(), 0.0)
+        self.assertEqual(transform.translate.get(), (0.0, 0.0, 0.0))
+        transform.translate = 10.0, 1.0, 0.0
+        # noinspection PyCallingNonCallable
+        self.assertEqual(transform.translate(), (10.0, 1.0, 0.0))
+        transform.tx.set(2.0)
+        self.assertEqual(transform.tx(), 2.0)
+        # find a node & it's shape
+        persp_transform = getNode('persp')
+        persp_shape = persp_transform.shape()
+        self.assertEqual(str(persp_shape), '|persp|perspShape')
+        # reparent a node
+        transform.setParent(persp_transform)
+        self.assertEqual(str(transform), '|persp|transform1')
+        self.assertEqual(transform.parent(), persp_transform)
+        # even attributes should still work
+        self.assertEqual(str(transform.translate), '|persp|transform1.translate')
+        # cmds return type validation
+        self.assertEqual(cmds.listRelatives(transform, ad=True), None)
+        self.assertEqual(cmds.xform(transform, q=True, ws=True, t=True), (2.0, 1.0, 0.0))
+        self.assertEqual(cmds.xform(transform, q=True, ws=True, m=True),
+                        (1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 2.0, 1.0, 0.0, 1.0))
+        self.assertEqual(cmds.file(rename='C:/Test.ma'), 'C:/Test.ma')
+        self.assertEqual('C:/Test.ma', cmds.file(q=True, sn=True))
+        self.assertEqual(['C:/Test.ma'], cmds.file(query=True, list=True))
+        self.assertEqual(['scale', 'scaleX', 'scaleY', 'scaleZ', 'scalePivot', 'scalePivotX', 'scalePivotY', 'scalePivotZ',
+                  'scalePivotTranslate', 'scalePivotTranslateX', 'scalePivotTranslateY', 'scalePivotTranslateZ'],
+                 cmds.listAttr(transform, r=True, st='scale*'))
+        self.assertEqual(True, cmds.objExists(transform))
+        self.assertEqual(DependNode, type(cmds.createNode('curveInfo')))
+        self.assertEqual(['|test', 'makeNurbCircle1'], [str(e) for e in cmds.circle(n='test')])
+        self.assertEqual('untitled', cmds.file(f=True, new=True))
+        self.assertEqual('y' or 'z', cmds.upAxis(q=True, axis=True))
+        cmds.joint()
+        cmds.joint()
+        cmds.joint()
+        self.assertEqual(children('|joint1'), {getNode('|joint1|joint2')})
+        self.assertEqual(allDescendants('|joint1'), set(getNode(('|joint1|joint2', '|joint1|joint2|joint3'))))
+        for inst in cmds.ls(sl=0)[::5]:
+            assert isinstance(inst, DependNode)
+        crc = cmds.circle()[0]
+        self.assertEqual(cmds.ls('%s[*]' % crc.cv, fl=True), getNode(['|nurbsCircle1.cv[%i]' % i for i in range(8)]))
+
+
+    def attributes(self):
+    	pass
+
+
+## \~english testing context that will fire the unitTests but can be used in a cProfile argument
+def testctx():
+    log_file = 'log_file.txt'
+    curDirect = os.path.dirname(__file__)
+    file = open(os.path.join(curDirect, log_file), "w")
+    suite = unittest.TestSuite()
+    result = unittest.TestResult()
+    suite.addTest(unittest.makeSuite(TestCmds))
+    runner = unittest.TextTestRunner(file)
+    test = runner.run(suite)
+    file.close()
+    if _coverage:
+        cov.stop()
+        cov.save()
+        cov.html_report()
+        
+    return test
+
+def runctx(inDef):
+    _canProfile = True
+    try:
+        from cmdWrapper import pyprof2calltree
+    except:
+        _canProfile = False
+    
+    pr = cProfile.Profile()
+    pr.enable()
+    result = inDef()
+    pr.disable()
+
+    if _canProfile:
+        currentBaseFolder = os.path.dirname(__file__)
+
+        baseLocation = os.path.normpath(os.path.join(currentBaseFolder, "qcachegrind"))
+        inLocation = os.path.normpath(os.path.join(currentBaseFolder, "unitTest"))
+
+        executable = os.path.normpath(os.path.join(baseLocation, "qcachegrind.exe"))
+        callGrindProf = os.path.normpath(os.path.join(inLocation, 'callgrind.profile'))
+        binaryData = os.path.normpath(os.path.join(inLocation, 'profiledRigSystem.profile'))
+
+        for path in [callGrindProf, binaryData]:
+            dirpath = os.path.dirname(path)
+            if not os.path.exists(dirpath):
+                os.makedirs(dirpath)
+
+        pr.dump_stats(binaryData)
+
+        pyprof2calltree.convert(pstats.Stats(binaryData), callGrindProf)
+        pyprof2calltree.visualize(pstats.Stats(binaryData))
+        subprocess.Popen([executable, callGrindProf])
+        return result
+    
+    pr.print_stats()
+    return result
+
+if __name__ == '__main__':
+    testObject = runctx(testctx)
+    print(" ============================ ")
+    print(testObject)
+    print("Test was succesfull : {}".format(testObject.wasSuccessful()))
+    print(" ============================ ")
+    sys.exit()
